52059c062c871dcf7bda66657e9dcb865632c065
sohan2day <74815503+sohan2day@users.noreply.github.com>
2021-01-03 00:49:34 -0800

    Add files via upload

---
+++ Project-4_freshness/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $('.main_menu').meanmenu({
+      meanMenuContainer:'.maindiv_area',
+      meanScreenWidth:991
+    });
+
+
+    }
+)
